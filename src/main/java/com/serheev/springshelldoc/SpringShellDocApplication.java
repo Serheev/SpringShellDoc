04cf72fea715f80d4afdd58f43e1e7d5d804dc47
Serheev <yserheev@gmail.com>
2024-02-24 17:15:49 -0800

    Initial commit

---
+++ src/main/java/com/serheev/springshelldoc/SpringShellDocApplication.java
@@ -0,0 +1,13 @@
+package com.serheev.springshelldoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShellDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShellDocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/serheev/springshelldoc/SpringShellDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serheev.springshelldoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShellDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
